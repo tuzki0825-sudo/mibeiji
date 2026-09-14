605e53532e16aee87dc1c7d13537895bbb39ed64
tuzki0825-sudo <tuzki0825@gmail.com>
2026-09-15 02:21:35 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mibeiji-v39';
+const CACHE='mibeiji-v41';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
