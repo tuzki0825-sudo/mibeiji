d03175f10c394a23b2ddb88fd73ff568689bd6ca
tuzki0825-sudo <tuzki0825@gmail.com>
2026-09-15 00:59:27 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,17 @@
-const CACHE="mibeiji-v27";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))).then(()=>self.skipWaiting()));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(xs=>Promise.all(xs.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{
-    const y=x.clone();
-    caches.open(CACHE).then(c=>c.put(e.request,y)).catch(()=>{});
-    return x;
-  })));
+const CACHE='mibeiji-v28';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET') return;
+  event.respondWith(fetch(req).then(res=>{
+    const copy=res.clone();
+    caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{});
+    return res;
+  }).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html'))));
 });
